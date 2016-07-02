0423c09e80c327aaa1a2378afeba5c44d3a2316d
ditam <ditam@users.noreply.github.com>
2016-07-02 14:16:06 +0200

    add RGB color bias to color generation

--- invaders.js
+++ invaders.js
@@ -1,7 +1,10 @@
 ﻿var PARAMS = Object.freeze({
   COLLECTION_TIME: 1000,
+  MAX_COLOR_BIAS: 3,
   MOVE_THRESHOLD: 200,
   MOVE_ADJUSTMENT_MULTIPLIER: 2,
+  INPUT_WIDTH: 300,
+  INPUT_HEIGHT: 250,
   CANVAS_WIDTH: 500,
   CANVAS_HEIGHT: 300,
   PIXEL_SIZE: 10
@@ -118,6 +121,16 @@ function hullMutator(invader, input){
   var basePixel = {
     color: getColorString(input.moveCount)
   };
+  var secondaryPixel = deepCopy(basePixel);
+  if(input.firstMove && input.lastMove){
+    var x0 = input.firstMove.x;
+    var y0 = input.firstMove.y;
+    var x1 = input.lastMove.x;
+    var y1 = input.lastMove.y;
+    if(Math.abs(x0-x1)<100 && Math.abs(y0-y1)<100){
+      secondaryPixel.color = getColorString(input.moveCount, 0.8, 0.8, 1)
+    }
+  }
   var eye = 0;
   if(input.firstMove && input.lastMove){
     //only color eyes if movement was downwards - should be rare due to layout
@@ -128,17 +141,25 @@ function hullMutator(invader, input){
     }
   }
   var part = [
-    [0,0,0,basePixel], // antenna will overwrite first 3 anyway
-    [basePixel,eye,basePixel,basePixel],
-    [basePixel,basePixel,basePixel,basePixel],
-    [basePixel,basePixel,basePixel,basePixel]
+    [0,0,0,secondaryPixel], // antenna will overwrite first 3 anyway
+    [secondaryPixel,eye,secondaryPixel,basePixel],
+    [basePixel,secondaryPixel,basePixel,secondaryPixel],
+    [secondaryPixel,basePixel,secondaryPixel,basePixel]
   ];
   return applyPart(invader, part, 2, 2);
 }
 
 function antennaMutator(invader, input){
+  var biasR = 1;
+  var biasG = 1;
+  var biasB = 1;
+  if(input.firstMove && input.lastMove){
+    biasR = input.firstMove.x/PARAMS.INPUT_WIDTH;
+    biasG = input.firstMove.y/PARAMS.INPUT_HEIGHT;
+    biasB = input.lastMove.x/PARAMS.INPUT_WIDTH;
+  }
   var basePixel = {
-    color: getColorString(input.moveCount)
+    color: getColorString(input.moveCount, biasR, biasG, biasB)
   };
   var part = [
     [basePixel,0,0,0],
@@ -166,8 +187,16 @@ function weaponMutator(invader, input){
 }
 
 function thrusterMutator(invader, input){
+  var biasR = 1;
+  var biasG = 1;
+  var biasB = 1;
+  if(input.lastMove){
+    var biasR = Math.min(PARAMS.MAX_COLOR_BIAS/2, input.lastMove.x/input.lastMove.y);
+    var biasG = input.lastMove.x > input.lastMove.y? 1: 0;
+    var biasB = (input.lastMove.x<20 && input.lastMove.y<20)? 0: 1;
+  }
   var basePixel = {
-    color: getColorString(input.moveCount)
+    color: getColorString(input.moveCount, biasR, biasG, biasB)
   };
   var part = [
     [basePixel,0,0,0],
@@ -176,10 +205,16 @@ function thrusterMutator(invader, input){
   return applyPart(invader, part, 2, 6);
 }
 
-function getColorString(moveCount){
+function getColorString(moveCount, biasR, biasG, biasB){
   var n = 256-Math.min(PARAMS.MOVE_THRESHOLD,moveCount*PARAMS.MOVE_ADJUSTMENT_MULTIPLIER);
-  //greyscale for now TODO: colorize with RGB bias param?
-  return 'rgb('+n+','+n+','+n+')';
+  var R = 1;
+  var G = 1;
+  var B = 1;
+  if(biasR) R=biasR;
+  if(biasG) G=biasG;
+  if(biasB) B=biasB;
+  //NB: non-integer values are not valid here!
+  return 'rgb('+Math.floor(n*R)+','+Math.floor(n*G)+','+Math.floor(n*B)+')';
 }
 
 function applyPart(base, part, x, y){
@@ -195,7 +230,6 @@ function applyPart(base, part, x, y){
 }
 
 function drawInvader(context, x0, y0, invader){
-  context.fillStyle = 'rgb(200,200,200)';
   context.clearRect(0,0,PARAMS.CANVAS_WIDTH,PARAMS.CANVAS_HEIGHT);
   
   for(var y=0;y<invader.length;y++){
